6d28df2e62579dde3c1303aedfaf90d62ef60504
PRINCEKUMARPRINCE9183 <priencepriencekumar3@gmail.com>
2025-04-16 02:44:38 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function gethistory(){
+    return document.getElementById["history-value"];
+}
+function gethistory(nun) {
+    return document.getElementById["history-value"];
+}
+function gethistory() {
+    return document.getElementById["output-value"];
+}
+function gethistory(nun) {
+    return document.getElementById["output-value"];
+}
+function.printoutput(nun)[
+    if(nun==""){
+        document.getElementById("output-value")
+    }
+]
+else{
+     document.getElementById("output-value");
+}
+function getformattednumber[nun]{
+    if{nun=="+"}{
+        return"";
+    }
+    var n=numner{nun};
+    var value = n.tolcalestring(en);
+    return value;
+}
+function reversenumberformat(nun){
+    return number[nun,replace(/,/g,'')];
+}
+var operator=document.getElementById
